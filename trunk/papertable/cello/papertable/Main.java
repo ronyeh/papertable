ce20d13261fcc602f171f0b347f797f9e9d8f6d5
marcello3d <marcello3d@28ab66be-4f31-0410-8979-97a32ceef242>
2007-05-25 19:03:28 +0000

    libraries and structure

---
+++ trunk/papertable/cello/papertable/Main.java
@@ -0,0 +1,21 @@
+package cello.papertable;
+
+import cello.papertable.ui.MainFrame;
+
+/**
+ * Main entry class
+ * 
+ * @author Marcello
+ *
+ */
+public class Main {
+
+	/**
+	 * Main entry point
+	 * @param args commandline arguments
+	 */
+	public static void main(String args[]) {
+		MainFrame m = new MainFrame();
+		m.setVisible(true);
+	}
+}
