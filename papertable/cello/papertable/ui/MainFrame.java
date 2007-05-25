ef5d1c7f76bb1280a2a12c7bb73368e0fcfd3895
marcello3d <marcello3d@28ab66be-4f31-0410-8979-97a32ceef242>
2007-05-25 19:03:28 +0000

    libraries and structure

---
+++ papertable/cello/papertable/ui/MainFrame.java
@@ -0,0 +1,26 @@
+package cello.papertable.ui;
+
+import javax.swing.JFrame;
+
+/**
+ * Main GUI window
+ * @author Marcello
+ */
+public class MainFrame extends JFrame {
+	/**
+	 * generated serial id
+	 */
+	private static final long serialVersionUID = -8431048183784216400L;
+
+	/**
+	 * Constructs a new MainFrame
+	 *
+	 */
+	public MainFrame() {
+		super("Papertable");
+		setSize(640,480);
+		setUndecorated(true);
+		
+	}
+
+}
